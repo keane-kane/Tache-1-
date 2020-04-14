8843bc9c8f53036a480976298eb3e609b8f1d781
keane-kane <51829744+keane-kane@users.noreply.github.com>
2020-04-14 20:22:09 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<!-----------------------------------code html------------------------>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/authentification.css">
+    
+    <title>Authantification Admin</title>
+</head>
+
+  <body>
+          <div class="plaisir">
+                <p class="imageH" ></p>
+                <h1 class="text jouer">Le plaisir de jouer</h1>
+          </div>
+          <div class="background fond">
+     <?php
+     
+     session_start();
+     require_once('traitement/fonction.php');
+
+    if(isset($_GET['page'])){
+        if($_GET['page']=='admin'){
+            require_once('interfaceAdmin.php');
+        }elseif($_GET['page']=='joueur'){
+            require_once('interfaceJoueur.php');
+        }
+    }else{
+        if(isset($_GET['status']) && $_GET['status']==="logout"){
+            deconnexion();
+        }
+    
+     require_once('authantification.php');
+    }
+     ?>
+ </div>
+  </body>
+</html>
