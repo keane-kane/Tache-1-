113f740d8a88187923cc9206a4a456714a9f7470
keane-kane <51829744+keane-kane@users.noreply.github.com>
2020-04-14 22:20:00 +0200

    Update interfaceJoueur.php

--- interfaceJoueur.php
+++ interfaceJoueur.php
@@ -1,30 +1,15 @@
-﻿<?php
-  session_start();
-  $nom = $_SESSION['nom'];
-  $prenom = $_SESSION['prenom'];
+<?php
+ is_connect();
+
+  $nom = $_SESSION['user']['nom'];
+  $prenom = $_SESSION['user']['prenom'];
  
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./styles/interfaceJoueur.css">
-    <title>Page Admin</title>
-    
-</head>
-  <body>
-   
-        <div class="plaisir">
-          <p class="imageH" ></p>
-          <h1 class="text jouer">Le plaisir de jouer</h1>
-        </div>
 
-        <div class="background fond">
             <div class="positionAdmin">
                 <div class="parametre">
                     <div class="avatar">
-                        <img src="Images/img-bg.jpg" alt="">
+                        
                         <div class="nompre">
                             <p class="prenomAdmin"><?= $prenom; ?></p>
                             <p class="nomAdmin"> <?= $nom; ?></p>
@@ -35,10 +20,8 @@
                     <div class="deconnection">
                     <?php
                         echo " <a onclick=\"return confirm('Vous souhaitez quitter votre session ?');\" 
-                              href='deconnexion.php'> Deconnexion</a>";
+                              href='index.php?status=logout'> Deconnexion</a>";
                      ?>
                     </div>
                </div>
             </div>
-    </body>
-</html>
