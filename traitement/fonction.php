e0a028b0c2304fc2d4b8e8e73f0c7a9f9e04fba2
keane-kane <51829744+keane-kane@users.noreply.github.com>
2020-04-14 22:24:09 +0200

    Update and rename traitement/fonction.php to  traitement/fonction.php

--- traitement/fonction.php
+++ traitement/fonction.php
@@ -1,1 +1,33 @@
-
+<?php 
+function connexion($login , $pwd){
+    $users= getDatas();
+    foreach($users as $key => $user){
+        if($user['login']=== $login && $user['password']===$pwd){
+            $_SESSION['user']= $user;
+            $_SESSION['status']= "login";
+            if($user['role']=== 'admin'){
+                return "admin";
+            }else //if($user['role']=== 'player')
+            {
+                return "joueur";
+               }
+        }
+    }
+    return "error";
+}
+ function is_connect(){
+    if(!isset($_SESSION['status'])){
+         header('location: index.php');
+    }
+}
+ function deconnexion(){
+    unset($_SESSION['user']);
+    unset($_SESSION['status']);
+    session_destroy();
+ }
+ function getDatas($file = 'ListesAd_Jou'){
+        $data = file_get_contents("fichierJson/".$file.".json");
+        $data = json_decode($data, true);
+        return $data;
+ }
+?>
